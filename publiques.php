993c6f36ab7abcedf2d1ef0f78f2153c6f719f06
Csfmht <95951487+Csfmht@users.noreply.github.com>
2021-12-11 05:33:01 +0100

    Add files via upload

---
+++ publiques.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <meta name="generator" content="Nicepage 3.13.2, nicepage.com">
+
+    <title>Publique_العموم</title>
+    <!----##############--------------------- Les liens avec les Styles et l'icone du site ----------------------##############-----> 
+      <link rel="stylesheet" href="css/nicepage.css" media="screen">
+      <link rel="stylesheet" href="css/publiques.css" media="screen">
+      <link id="u-theme-google-font" rel="stylesheet" href="css/fonts.css">
+
+      <link rel="icon" href="images/favicon3.ico">
+    <!----##############--------------------- Les liens avec les JavaScript ----------------------##############-----> 
+    
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+
+    
+
+    
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "CSFMH Tabarka",
+		"url": "index.php",
+		"logo": "images/ATFPFlags.gif",
+		"sameAs": []
+    }</script>
+    <meta property="og:title" content="Publique_العموم">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.php">
+    <meta property="og:url" content="index.php">
+    <script>
+    function gdprConfirmed() {
+        return document.cookie.split(';').filter(function(item) {
+            return item.trim().indexOf('u-gdpr-cookie=true') >= 0
+        }).length;
+    }
+    if (gdprConfirmed()) {
+    document.write("\
+    \
+    <!-- Google Analytics -->\
+    <gascript async src=\"https://www.googletagmanager.com/gtag/js?id=AIzaSyAv7h2waVN7fI349svwE-0jiHxLxUnD6Tg\"></gascript>\
+    <gascript>\
+        window.dataLayer = window.dataLayer || [];\
+        function gtag(){dataLayer.push(arguments);}\
+        gtag('js', new Date());\
+        gtag('config', 'AIzaSyAv7h2waVN7fI349svwE-0jiHxLxUnD6Tg');\
+    </gascript>\
+    <!-- End Google Analytics -->\
+    ".replace(/gascript/g, 'script'));
+    }
+    </script>
+  </head>
+  <body class="u-body u-stick-footer">
+      <!----------------------##############--------------------- entet du page ----------------------##############--------------------->
+      <?php include("entetdepages.php");?>
+      <!----------------------##############--------------------- corps du page ----------------------##############--------------------->
+    <section class="u-align-left u-clearfix u-image u-image-contain u-section-1" src="" id="sec-43b5" data-image-width="1400" data-image-height="400">
+      <div class="u-clearfix u-sheet u-sheet-1"></div>
+    </section>
+    <section class="u-clearfix u-grey-25 u-section-2" id="carousel_942f">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-container-style u-layout-cell u-size-34 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h3 class="u-align-left u-text u-text-1">A Propos</h3>
+                  <p class="u-align-justify u-text u-text-2">
+                    Le Centre Sectoriel de Formation en Maintenance Hôtelière à Tabarka&nbsp;CSFMHT&nbsp;créé en 1998 est un établissement public de formation s’appuyant sur un établissement principal l'Agence Tunisienne de la Formation Professionnelle&nbsp;ATFP&nbsp;basé à Tunis. Le Centre dispose d’un ensemble de moyens, de savoir-faire et de compétences mobilisés pour assurer une formation initiale et contenue dans les domaines de l’énergétique, l’électricité et de l’électronique </p>
+                  <h3 class="u-align-right u-text u-text-3">حول</h3>
+                  <p class="u-align-right u-custom-font u-font-arial u-text u-text-4"><b>المركز القطاعي للتكوين في الصيانة الفندقية بطبرقة أسس سنة 1998 وهو مؤسسة عمومية تحت إشراف الوكالة التونسية للتكوين المهني بتونس. يتمتع المركز بمجموعة من الوسائل، المعارف والمهارات لتوفير تكوين أساسي أو مستمر في ميادين الطاقة، الكهرباء والإلكترونيك وهو قطاع تحتاجه كل البناءات وخاصة منها الجماعية مثل النزل، المستشفيات، المصانع والمجمعات التجارية</b>
+                  </p>
+                </div>
+              </div>
+              <div class="u-container-style u-layout-cell u-size-26 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <div class="u-expanded-width-sm u-expanded-width-xs u-gradient u-shape u-shape-rectangle u-shape-1"></div>
+                  <img src="images/IMG_20210201_093746.jpg" alt="" class="u-align-left u-image u-image-default u-image-1" data-image-width="4608" data-image-height="3456" data-animation-name="shake" data-animation-duration="1000" data-animation-delay="0" data-animation-direction="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-grey-10 u-section-3" id="sec-4cc9">
+      <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-sheet-1">
+        <div id="carousel-0ef4" data-interval="5000" data-u-ride="carousel" class="u-carousel u-expanded-width u-slider u-slider-1">
+          <ol class="u-absolute-hcenter u-carousel-indicators u-carousel-indicators-1">
+            <li data-u-target="#carousel-0ef4" class="u-active u-gradient" data-u-slide-to="0" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="1" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="2" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="3" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="4" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="5" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="6" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="7" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+            <li data-u-target="#carousel-0ef4" class="u-gradient" data-u-slide-to="8" style="background-image: linear-gradient(#478ac9, #4c7397);"></li>
+          </ol>
+          <div class="u-carousel-inner" role="listbox">
+            <div class="u-active u-align-left u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-valign-middle-xs u-container-layout-1">
+                <img src="images/AEC.jpg" alt="" class="u-expanded-width-xs u-image u-image-default u-image-1" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-2">
+                <img src="images/ITS.jpg" alt="" class="u-image u-image-default u-image-2" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-3">
+                <img src="images/MDF.jpg" alt="" class="u-image u-image-default u-image-3" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-4">
+                <img src="images/TFCC.jpg" alt="" class="u-image u-image-default u-image-4" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-5">
+                <img src="images/TGTC.jpg" alt="" class="u-image u-image-default u-image-5" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-6">
+                <img src="images/TIMSAV.jpg" alt="" class="u-image u-image-default u-image-6" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-7">
+                <img src="images/TMC.jpg" alt="" class="u-image u-image-default u-image-7" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-8">
+                <img src="images/TMCPB.jpg" alt="" class="u-image u-image-default u-image-8" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+            <div class="test-slide u-carousel-item u-container-style u-slide">
+              <div class="u-container-layout u-container-layout-9">
+                <img src="images/TSC.jpg" alt="" class="u-image u-image-default u-image-9" data-image-width="1280" data-image-height="720">
+              </div>
+            </div>
+          </div>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-prev u-spacing-5 u-text-grey-30 u-carousel-control-1" href="#carousel-0ef4" role="button" data-u-slide="prev">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225
+                    c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"></path></svg>
+            </span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="u-absolute-vcenter u-carousel-control u-carousel-control-next u-spacing-5 u-text-grey-30 u-carousel-control-2" href="#carousel-0ef4" role="button" data-u-slide="next">
+            <span aria-hidden="true">
+              <svg viewBox="0 0 477.175 477.175"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5
+                    c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"></path></svg>
+            </span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <a href="http://41.231.253.195/candidate/login" class="u-btn u-btn-round u-button-style u-hover-palette-4-base u-palette-3-base u-radius-50 u-btn-1" target="_blank">Inscrivez-vous auprès de l'ATFP<br>سجل عبر بوابة الوكالة التونسية للتكوين المهني
+        </a>
+      </div>
+    </section>
+    <section class="u-align-center u-clearfix u-grey-25 u-section-4" id="sec-e142">
+      <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-size-30">
+                <div class="u-layout-col">
+                  <div class="u-align-left u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+                    <div class="u-container-layout u-container-layout-1">
+                      <h2 class="u-text u-text-1">CSFMH TABARKA</h2>
+                      <p class="u-text u-text-2">Bp 348, Route de Tunis Km 7, Tabarka 8110</p>
+                      <p class="u-text u-text-3">(+) 216 78672 030</p>
+                      <p class="u-text u-text-4">CSFMH.Tabarka@takwin.atfp.tn</p>
+                      <h2 class="u-align-right u-text u-text-5">
+                        <span style="font-size: 1.875rem;">هل سبق لك وزرت المركز؟&nbsp;</span>
+                        <br>
+                      </h2>
+                      <h2 class="u-align-left u-text u-text-6">
+                        <span style="font-size: 1.25rem;">Avez-vous déjà visité le centre? </span>
+                        <br>
+                      </h2>
+                      <p class="u-align-right u-text u-text-7">&nbsp;!شارك في تحسين خدمات المركز</p>
+                      <p class="u-align-left u-text u-text-8">Participez à l'amélioration des services du centre! </p>
+                      <a href="https://forms.gle/avhroyuCvbMeoeBRA" class="u-btn u-btn-round u-button-style u-hover-palette-1-light-1 u-palette-1-base u-radius-6 u-btn-1" target="_blank">Sondage إستبيان</a>
+                    </div>
+                  </div>
+                  <div class="u-align-left u-container-style u-image u-image-contain u-layout-cell u-left-cell u-shading u-size-30 u-image-1" data-image-width="150" data-image-height="100">
+                    <div class="u-container-layout u-valign-bottom u-container-layout-2">
+                      <div class="u-form u-form-1">
+                        <form action="#" method="POST" class="u-clearfix u-form-spacing-3 u-form-vertical u-inner-form" style="padding: 13px;" source="custom" name="form-5">
+                          <div class="u-form-group u-form-name u-form-group-1">
+                            <label for="name-5359" class="u-form-control-hidden u-label">Name</label>
+                            <input type="text" placeholder="Name" id="name-5359" name="name" class="u-border-3 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-black u-input-1" required="">
+                          </div>
+                          <div class="u-form-email u-form-group u-form-group-2">
+                            <label for="email-5359" class="u-form-control-hidden u-label">Email</label>
+                            <input type="email" placeholder="Email" id="email-5359" name="email" class="u-border-3 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-black u-input-2" required="">
+                          </div>
+                          <div class="u-form-group u-form-message u-form-group-3">
+                            <label for="message-5359" class="u-form-control-hidden u-label"></label>
+                            <textarea placeholder="Votre Message, Idées..." rows="4" cols="50" id="message-5359" name="message" class="u-border-3 u-border-no-left u-border-no-right u-border-no-top u-border-white u-input u-input-rectangle u-text-black u-input-3" required=""></textarea>
+                          </div>
+                          <div class="u-form-group u-form-submit u-form-group-4">
+                            <a href="#" class="u-btn u-btn-submit u-button-style">Postuler</a>
+                            <input type="submit" value="submit" class="u-form-control-hidden">
+                          </div>
+                          <div class="u-form-send-message u-form-send-success">Thank you! Your message has been sent.</div>
+                          <div class="u-form-send-error u-form-send-message">Unable to send your message. Please fix errors then try again.</div>
+                          <input type="hidden" value="" name="recaptchaResponse">
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="u-size-30">
+                <div class="u-layout-col">
+                  <div class="u-align-left u-container-style u-layout-cell u-right-cell u-size-60 u-layout-cell-3">
+                    <div class="u-container-layout u-container-layout-3">
+                      <div class="u-expanded u-grey-light-2 u-map">
+                        <div class="embed-responsive">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1896.0577691456097!2d8.810264571569267!3d36.9435110048949!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fb35880ab0ce37%3A0x25e460f79f38918e!2sCSFMH%20Tabarka!5e0!3m2!1sfr!2stn!4v1619353155319!5m2!1sfr!2stn" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <style class="u-overlap-style">.u-overlap:not(.u-sticky-scroll) .u-header {
+background-image: linear-gradient(#292e33, #478ac9) !important
+}</style>
+      <!----------------------##############--------------------- Pied du page ----------------------##############--------------------->
+        <?php include("pieddepage.php");?>
+  </body>
+</html>
